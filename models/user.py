d33a45dedd7c2fc92ca1018fa696442ba52794c7
Roberto <robpalacios1@hotmail.com>
2020-08-12 11:13:45 -0500

    update add user.py file

--- models/user.py
+++ models/user.py
@@ -18,5 +18,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    reviews = relationship("Review", backref="user")
