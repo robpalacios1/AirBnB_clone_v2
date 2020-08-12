c0eabb43646fdc447299327efeaf97c44df742ab
josebant29 <josebant29@gmail.com>
2020-08-12 15:28:40 -0500

    cascade

--- models/user.py
+++ models/user.py
@@ -20,5 +20,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", cascade='delete', backref="user")
-    reviews = relationship("Review", cascade='delete', backref="user")
+    places = relationship("Place", backref="user", cascade='all', passive_deletes=True)
+    reviews = relationship("Review", backref="user", cascade='delete')
