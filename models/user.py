abf3f9fbaa8330d8bce5e726555e14c2325c2cd3
Roberto <robpalacios1@hotmail.com>
2020-08-13 11:34:11 -0500

    update user.py file

--- models/user.py
+++ models/user.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.place import Place
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-        Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    
+    """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade='all', passive_deletes=True)
-    reviews = relationship("Review", backref="user", cascade='delete')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete')
+        reviews = relationship('Review', backref='user', cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
