2bd0e3d2266fd5e27a305f2d0964545d1d20e51e
josebant29 <josebant29@gmail.com>
2020-08-13 23:19:50 -0500

    amenity

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from os import getenv
+from models.amenity import Amenity
 
 
 place_amenity = Table('place_amenity', Base.metadata,
