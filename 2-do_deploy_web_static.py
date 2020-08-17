734073498ba30d8e108f4f11f053a537ce575288
Roberto <robpalacios1@hotmail.com>
2020-08-17 15:03:57 -0500

    add script and convert to exec file

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+deploys archive to web servers
+"""
+from fabric.operations import put, run, env
+import os
+
+env.hosts = ['35.237.35.49', '34.73.18.193']
+
+
+def do_deploy(archive_path):
+    if os.path.exists(archive_path) is False:
+        return False
+    else:
+        put(archive_path, "/tmp/")
+        tgzfile = archive_path.split('/')[1]
+        archivedir = tgzfile.split('.')[0]
+        run("mkdir -p /data/web_static/releases/{}/"
+            .format(archivedir))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, archivedir))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/"
+            .format(archivedir, archivedir))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(archivedir))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archivedir))
+        return True
