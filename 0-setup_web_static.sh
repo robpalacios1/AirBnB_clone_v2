bdb71f2ffa74a4674c9e7c9fead42f28f14586e2
Roberto <robpalacios1@hotmail.com>
2020-08-17 13:39:38 -0500

    add new files to the repo

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# set up servers to deploy web static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "41i \\\nlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
+sudo service nginx restart
