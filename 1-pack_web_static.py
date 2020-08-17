53b60651acec4fbcc3de6638e901f82f3d753036
Roberto <robpalacios1@hotmail.com>
2020-08-17 14:15:14 -0500

    fix code inside emacs

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""uses fabric to pack web_static into file"""
-
+"""
+uses fabric to pack web_static into file
+"""
 from fabric.operations import local
 import os
 from datetime import datetime
@@ -10,9 +11,9 @@ def do_pack():
     try:
         if os.path.exists("versions") is False:
             local("mkdir versions")
-        created = datetime.now().strftime("%Y%m%d%H%M%S")
-        tgzfile = "versions/web_static_{}.tgz".format(created)
-        local("tar -cvzf {} web_static".format(tgzfile))
-        return tgzfile
+            created = datetime.now().strftime("%Y%m%d%H%M%S")
+            tgzfile = "versions/web_static_{}.tgz".format(created)
+            local("tar -cvzf {} web_static".format(tgzfile))
+            return tgzfile
     except:
         return None
