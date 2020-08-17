60513d9d03a81e165275155fe4b4adb39ef54ee2
Roberto <robpalacios1@hotmail.com>
2020-08-17 13:59:57 -0500

    add script and convert to exec file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""uses fabric to pack web_static into file"""
+
+from fabric.operations import local
+import os
+from datetime import datetime
+
+
+def do_pack():
+    try:
+        if os.path.exists("versions") is False:
+            local("mkdir versions")
+        created = datetime.now().strftime("%Y%m%d%H%M%S")
+        tgzfile = "versions/web_static_{}.tgz".format(created)
+        local("tar -cvzf {} web_static".format(tgzfile))
+        return tgzfile
+    except:
+        return None
