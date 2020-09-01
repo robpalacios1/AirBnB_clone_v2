e804317db95810fa572a510dcaf3583fe586e5d0
Roberto <robpalacios1@hotmail.com>
2020-09-01 15:57:57 -0500

    fix code inside emacs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,19 +41,7 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
