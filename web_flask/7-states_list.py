02ea500ff4033ea58a68511b5628d2cf7eb6b7c0
Roberto <robpalacios1@hotmail.com>
2020-09-01 16:21:04 -0500

    fix code inside emacs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+"""List all states Module"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """display a HTML the States"""
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
