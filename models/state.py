825168b0c65139c34791824ad2c6644b73cf96ba
Roberto <robpalacios1@hotmail.com>
2020-09-01 17:43:33 -0500

    fix code inside emacs

--- models/state.py
+++ models/state.py
@@ -2,32 +2,33 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+import models
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship, backref
 from os import getenv
-import models
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", cascade="all, delete-orphan",
-                              backref="state")
+    """ State class """
 
-    else:
-        """File Storage"""
+    __tablename__ = "states"
+
+    name = Column(String(128),
+                  nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City",
+                              backref=backref("state", cascade='all'),
+                              cascade="all, delete-orphan",
+                              single_parent=True)
+
+    if getenv("HBNB_TYPE_STORAGE") == "fs":
         @property
         def cities(self):
-            """
-            city properties
-            """
-            city = []
-            for val in models.storage.all(City).values():
-                if val.state_id == self.id:
-                    city.append(val)
-            return city
+            """Return the list of City instances with state_id """
+            from models import storage
+            city_list = []
+            for ct in models.storage.all(City).values():
+                if ct.state_id == self.id:
+                    city_list.append(ct)
+            return (city_list)
