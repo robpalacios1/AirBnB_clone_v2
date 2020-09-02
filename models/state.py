2a02c2bab018c4ef30af8545e228a007e970cb55
Roberto <robpalacios1@hotmail.com>
2020-09-02 11:42:56 -0500

    fix code inside emacs

--- models/state.py
+++ models/state.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""State Module for HBNB project."""
-import models
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
-from os import getenv
+from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
-    """State class."""
-
+    """This is the class for State
+    Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
         @property
         def cities(self):
-            """Return list of Cities with the current state_id."""
-            lt_cities = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
+            from models import storage
+            from models.city import City
+            # return list of City objs in __objects
+            cities_dict = storage.all(City)
+            cities_list = []
+
+            # copy values from dict to list
+            for city in cities_dict.values():
                 if city.state_id == self.id:
-                    lt_cities.append(city)
-            return lt_cities
+                    cities_list.append(city)
+
+            return cities_list
