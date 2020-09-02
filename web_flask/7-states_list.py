5cb02381283abf551cda2d3434069a4fc6593eae
Roberto <robpalacios1@hotmail.com>
2020-09-02 11:33:47 -0500

    fix code inside emacs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,24 +8,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list')
-def states_list():
-    """Render template with states
-    """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    # sort State object alphabetically by name
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
-
-
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(exception):
+    """Removes current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/states_list", strict_slashes=False)
+def route1():
+    """Display HTML page with a list of
+    all objects inside a DBstorage."""
+    url = '7-states_list.html'
+    dictionary = storage.all("State")
+    return render_template(url, states=dictionary)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
