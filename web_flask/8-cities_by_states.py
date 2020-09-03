041381df9f3d26416aa4ac3c17f7f89685207065
Roberto <robpalacios1@hotmail.com>
2020-09-02 19:31:44 -0500

    fix code inside emacs

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
+"""List cities by states Module"""
 
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/cities_by_states')
 def states_list():
-    """Render template with states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
+    """display a HTML the States and cities by states"""
+    all_states = list(storage.all(State).values())
+    return (render_template('8-cities_by_states.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
